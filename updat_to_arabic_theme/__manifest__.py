f95ce51e2b81b6a0d0bf665126a4d687473ee5eb
Abdulrahman Hamdy <ahamdy@profittools.net>
2020-09-08 16:11:11 +0400

    Initial commit

---
+++ updat_to_arabic_theme/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    # Theme information
+    'name': 'Update to arabic Theme ',
+    'summary': 'change some point to arabic ',
+    'depends': [
+    ],
+
+    'data': [
+        'templates/asset.xml',
+        'templates/web_layout.xml',
+        'templates/website_sale.xml',
+    ],
+
+
+    # Author
+    'author': 'Trend',
+
+    # Technical
+    'installable': True,
+    'auto_install': False,
+}
