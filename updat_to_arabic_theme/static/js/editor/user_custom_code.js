fe0324e54b7f536c231e5247a8f8d34891800d42
root <enasyasser38@gmail.com>
2020-09-12 22:02:37 +0200

    Update

--- updat_to_arabic_theme/static/js/editor/user_custom_code.js
+++ updat_to_arabic_theme/static/js/editor/user_custom_code.js
@@ -4,8 +4,7 @@ odoo.define('updat_to_arabic_theme.user_custom_code', function (require) {
 'use strict';
 
 $(document).ready(function(){
-  console.log('load-----')
-
+ 
      document.getElementById("fixed_img").style.display = "none";
 
   })
