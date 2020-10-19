b7ee91245f3e570428cb2142acd34563535abf61
root <enasyasser38@gmail.com>
2020-10-19 13:41:44 +0200

    update

--- theme_fgalaa/model/res_company.py
+++ theme_fgalaa/model/res_company.py
@@ -3,8 +3,20 @@ from odoo.exceptions import ValidationError
 
 class ResCompany(models.Model):
     _inherit = 'res.company'
-    
-    
+
+    branch_name7 = fields.Char(string='7th branch name')
+    place7_lat = fields.Char(string='7th branch Latitude')
+    place7_long = fields.Char(string='7th branch Longitude')
+
+    branch_name8 = fields.Char(string='8th branch name')
+    place8_lat = fields.Char(string='8th branch Latitude')
+    place8_long = fields.Char(string='8th branch Longitude')
+
+    branch_name6 = fields.Char(string='6th branch name')
+    place6_lat = fields.Char(string='6th branch Latitude')
+    place6_long = fields.Char(string='6th branch Longitude')
+
+
     branch_name1= fields.Char(string='First branch name')
     place1_lat= fields.Char(string='First branch Latitude')
     place1_long= fields.Char(string='First branch Longitude')
@@ -25,11 +37,3 @@ class ResCompany(models.Model):
     place5_lat = fields.Char(string='Fifth branch Latitude')
     place5_long = fields.Char(string='Fifth branch Longitude')
 
-
-    branch_name7 = fields.Char(string='7th branch name')
-    place7_lat = fields.Char(string='7th branch Latitude')
-    place7_long = fields.Char(string='7th branch Longitude')
-
-    branch_name8 = fields.Char(string='8th branch name')
-    place8_lat = fields.Char(string='8th branch Latitude')
-    place8_long = fields.Char(string='8th branch Longitude')
