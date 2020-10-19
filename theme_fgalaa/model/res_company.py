d287ad88b5e6c047604cd4232c613485048ffd9e
root <enasyasser38@gmail.com>
2020-10-19 13:25:29 +0200

    update

--- theme_fgalaa/model/res_company.py
+++ theme_fgalaa/model/res_company.py
@@ -22,17 +22,17 @@ class ResCompany(models.Model):
     place4_long = fields.Char(string='Fourth branch Longitude')
 
     branch_name5 = fields.Char(string='Fifth branch name')
-    place5_lat= fields.Char(string='Fifth branch Latitude')
+    place5_lat=  fields.Char(string='Fifth branch Latitude')
     place5_long = fields.Char(string='Fifth branch Longitude')
 
-    branch_name6 = fields.Char(string='Fifth branch name')
-    place6_lat = fields.Char(string='Fifth branch Latitude')
-    place6_long = fields.Char(string='Fifth branch Longitude')
+    branch_name6 = fields.Char(string='6th branch name')
+    place6_lat = fields.Char(string='6th branch Latitude')
+    place6_long = fields.Char(string='6th branch Longitude')
 
-    branch_name7 = fields.Char(string='Fifth branch name')
-    place7_lat = fields.Char(string='Fifth branch Latitude')
-    place7_long = fields.Char(string='Fifth branch Longitude')
+    branch_name7 = fields.Char(string='7th branch name')
+    place7_lat = fields.Char(string='7th branch Latitude')
+    place7_long = fields.Char(string='7th branch Longitude')
 
-    branch_name8 = fields.Char(string='Fifth branch name')
-    place8_lat = fields.Char(string='Fifth branch Latitude')
-    place8_long = fields.Char(string='Fifth branch Longitude')
+    branch_name8 = fields.Char(string='8th branch name')
+    place8_lat = fields.Char(string='8th branch Latitude')
+    place8_long = fields.Char(string='8th branch Longitude')
