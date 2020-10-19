854794e1f9006f275d79a8e54a9cecc520a587c0
root <enasyasser38@gmail.com>
2020-10-19 13:30:49 +0200

    update

--- theme_fgalaa/model/res_company.py
+++ theme_fgalaa/model/res_company.py
@@ -29,10 +29,10 @@ class ResCompany(models.Model):
     place6_lat = fields.Char(string='6th branch Latitude')
     place6_long = fields.Char(string='6th branch Longitude')
 
-    # branch_name7 = fields.Char(string='7th branch name')
-    # place7_lat = fields.Char(string='7th branch Latitude')
-    # place7_long = fields.Char(string='7th branch Longitude')
-    #
-    # branch_name8 = fields.Char(string='8th branch name')
-    # place8_lat = fields.Char(string='8th branch Latitude')
-    # place8_long = fields.Char(string='8th branch Longitude')
+    branch_name7 = fields.Char(string='7th branch name')
+    place7_lat = fields.Char(string='7th branch Latitude')
+    place7_long = fields.Char(string='7th branch Longitude')
+
+    branch_name8 = fields.Char(string='8th branch name')
+    place8_lat = fields.Char(string='8th branch Latitude')
+    place8_long = fields.Char(string='8th branch Longitude')
